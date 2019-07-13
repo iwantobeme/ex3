217de0cd0ffd622fd3d5385b7509ef340a4f0bbd
Panuwat Yuenyong <59143204@g.cmru.ac.th>
2019-07-13 16:10:23 +0700

    aaas

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@
 
 var express = require('express');
 var router = express.Router();
+var connect = require('../config/database')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fucking Web' });
+  var query = "SELECT * FROM positions";
+  connect.query(query,function(error,result){
+    res.json(result)
+    
+  })
 });
 router.get('/about', function(req, res, next) {
   res.render('about', { title: "Let's Git it Web" });
